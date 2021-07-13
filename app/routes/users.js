1d9739d4fac29945a6dfb44c945af95c9a3a3f33
= <=>
2021-07-13 14:17:05 -0300

    inserindo a rota de registro de usuario

--- app/routes/users.js
+++ app/routes/users.js
@@ -1,9 +1,18 @@
 var express = require('express');
 var router = express.Router();
+const user = require("../models/user")
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+router.post('/register', async (req, res) =>{
+  const {name, email, password} = req.body
+  const user = new User({name, email, passowrd})
+
+  try{
+    await user.save()
+    res.status(200).json(user)
+  }catch(error){
+    res.status(500).json({error: "Error registering new user !"})
+  }
+})
 
 module.exports = router;
