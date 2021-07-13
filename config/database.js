06c75b86ac167989b10460f6d6d9c0137507a124
= <=>
2021-07-13 11:24:08 -0300

    Inserindo o projeto do clone do evernote

---
+++ config/database.js
@@ -0,0 +1,9 @@
+var mongoose = require("mongoose")
+mongoose.Promise = global.Promise
+
+mongoose.connect('mongodb://localhost/javascriptNote',{
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true
+}).then(() => console.log("Conectado com sucesso !!!"))
+.catch(err => console.log(err))
