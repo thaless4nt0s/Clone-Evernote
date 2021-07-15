31225f4610e3f1417ec180311586a4cf50b18aee
= <=>
2021-07-15 11:03:32 -0300

    Script de pesquisa de uma nota especifica

--- app/models/note.js
+++ app/models/note.js
@@ -3,13 +3,15 @@ const mongoose = require('mongoose')
 let noteSchema = new mongoose.Schema({
     title: String,
     body: String,
-    created_At: {type: Date, default: Date.now},
-    updated_At: {type: Date, defulta: Date.now},
-    author:{
+    created_At: { type: Date, default: Date.now },
+    updated_At: { type: Date, defulta: Date.now },
+    author: {
         type: mongoose.Schema.Types.ObjectId,
         ref: 'User',
         required: true
     }
 })
 
-module.exports = mongoose.model('Note',noteSchema)
+noteSchema.index({ 'title': 'text', 'body': 'text' })
+
+module.exports = mongoose.model('Note', noteSchema)
--- app/routes/notes.js
+++ app/routes/notes.js
@@ -14,68 +14,79 @@ router.post("/", WithAuth, async (req, res) => {
     }
 })
 
+router.get("/search", WithAuth, async (req, res) => {
+    const { query } = req.query
+    try {
+        let notes = await Note.find({ author: req.user._id }).find({ $text: { $search: query } })
+        res.status(200).json(notes)
+
+    } catch (error) {
+        res.status(500).json({ error: error })
+    }
+})
+
 router.get("/:id", WithAuth, async (req, res) => {
-    try{    
-        const {id} = req.params
+    try {
+        const { id } = req.params
         let note = await Note.findById(id)
-        if(isOwner(req.user, note)){
+        if (isOwner(req.user, note)) {
             res.status(200).json(note)
-        }else{
+        } else {
             res.status(500).json({ error: 'Permission denied to access ' })
         }
-    }catch(error){
-        res.status(500).json({ error: "Problem to get a note !! "})
+    } catch (error) {
+        res.status(500).json({ error: "Problem to get a note !! " })
     }
 })
 
-router.get('/', WithAuth, async (req, res) =>{
-    try{
-        let notes = await Note.find({author: req.user._id})
+router.get('/', WithAuth, async (req, res) => {
+    try {
+        let notes = await Note.find({ author: req.user._id })
         res.json(notes)
-    }catch(error){
-        res.status(500).json({ error: "Problem to  get all notes !! "})
+    } catch (error) {
+        res.status(500).json({ error: "Problem to  get all notes !! " })
     }
 })
 
-router.put("/:id", WithAuth, async (req, res) =>{
-    try{
-        const {title, body} = req.body
-        let {id} = req.params
+router.put("/:id", WithAuth, async (req, res) => {
+    try {
+        const { title, body } = req.body
+        let { id } = req.params
         let note = await Note.findById(id)
-        if(isOwner(req.user, note)){
+        if (isOwner(req.user, note)) {
             let note = await Note.findOneAndUpdate(id,
-                {$set: {title: title, body: body}},
-                {upsert: true, 'new': true}    
+                { $set: { title: title, body: body } },
+                { upsert: true, 'new': true }
             )
             res.json(note)
-        }else{
-            res.status(500).json({error: "Permission denied to update a note !!"})
+        } else {
+            res.status(500).json({ error: "Permission denied to update a note !!" })
         }
-    }catch(error){
-        res.status(500).json({error: "Problem to update a note !!"})
+    } catch (error) {
+        res.status(500).json({ error: "Problem to update a note !!" })
     }
 })
 
-router.delete("/:id", WithAuth, async (req, res) =>{
-    let {id} = req.params;
-    try{
+router.delete("/:id", WithAuth, async (req, res) => {
+    let { id } = req.params;
+    try {
         let note = await Note.findById(id)
-        if(isOwner(req.user, note)){
+        if (isOwner(req.user, note)) {
             await note.delete()
-            res.json({message: "OK"}).status(204)
-        }else{
-            res.status(500).json({error: "Permission Denied !!"})
+            res.json({ message: "OK" }).status(204)
+        } else {
+            res.status(500).json({ error: "Permission Denied !!" })
         }
-    }catch(error){
-        res.status(500).json({error: "Problem to delete a note !!"})
+    } catch (error) {
+        res.status(500).json({ error: "Problem to delete a note !!" })
     }
 })
 
 //Caso ele seja dono da nota retornará verdadeiro
-const isOwner = (user, note) =>{
-    if(JSON.stringify(user._id) == JSON.stringify(note.author._id)){
+const isOwner = (user, note) => {
+    if (JSON.stringify(user._id) == JSON.stringify(note.author._id)) {
         return true
-    }else{
+    } else {
         return false
     }
 }
